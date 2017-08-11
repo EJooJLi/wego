3811bd653c83ee51551dcb848633ff61775d68f3
James Li <james.li@opower.com>
2017-08-11 00:40:55 -0700

    Specifying user schema and setting up exports

--- app/Model/Schemas/user.js
+++ app/Model/Schemas/user.js
@@ -1,1 +1,23 @@
 //User schema
+//This creates a User schema using Schema in mongoose
+
+//First we import the modules needed
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+//Then we declare a User Schema to identify the actual format of the data
+
+var UserSchema = new Schema({
+  id: {type: Number},
+  firstname: {type: String},
+  lastname: {type: String},
+  username: {type: String},
+  password: {type: String},
+  location: {type: String},
+  updated_at: {type: Date, required: true, default: Date.now}
+});
+
+//Now we set a model using the UserSchema that can be used by our app
+var User = mongoose.model('User', UserSchema);
+//Export the Schema as User that can be required by our app
+module.exports = User;
