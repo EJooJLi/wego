6a3390ea20fabbdb7b6e091170fd17e78706aadb
James Li <james.li@opower.com>
2017-08-11 17:03:57 -0700

    updating app.js

--- app/Controller/Routes/userRoutes.js
+++ app/Controller/Routes/userRoutes.js
@@ -1,5 +1,5 @@
 // This is for user routing which requires functions created in controller/user.js
-var UserControl = require('../controller/user');
+var UserControl = require('../user');
 
 // Specifying API Server Endpoints, each corresponds to a directory path
 // and a controller CRUD function & exporting this router
