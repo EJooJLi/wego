86889563b00afc6016aa5955a1403ca7dced0841
Elliott Joo <elliott.joo@oracle.com>
2017-08-15 12:23:55 -0700

    signup

---
+++ app/View/src/app/signup.component.ts
@@ -0,0 +1,30 @@
+//This Component is for the header of the page.
+import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'userdata',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./styles.css']
+
+})
+
+export class SignUpComponent {
+
+  userinfo="";
+
+  getinfo(username: string, email: string, password: string) {
+    this.userinfo= JSON.stringify([username, email, password]);
+  }
+
+  //BEGIN HTTP STUFF
+  apiRoot: string = "http://httpbin.org";
+  constructor(private http: HttpClient) { }
+
+  doPOST() {
+    console.log("POST");
+    let url = `${this.apiRoot}/post`;
+    this.http.post(url, this.userinfo).subscribe(res => console.log(res));
+  }
+  //END HTTP STUFF
+}
