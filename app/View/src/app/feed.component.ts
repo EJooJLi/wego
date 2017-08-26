6994e6594fb11dd6b077159b8898e25e6c4e1314
Elliott Joo <elliott.joo@oracle.com>
2017-08-25 17:50:51 -0700

    sorting

--- app/View/src/app/feed.component.ts
+++ app/View/src/app/feed.component.ts
@@ -12,26 +12,18 @@ import { Component } from '@angular/core';
 export class FeedComponent {
 
   testcards =
-    {
-    0: {title: "How about a hike?", date: "2017-08-03", content: "Cool weather, cloudy skies, beautiful views at Yosemite"},
-    2: {title: "Visit the museum?", date: "2017-08-05", content: "Rainy days don't have to ruin your plans!"},
-    1: {title: "Free concert!", date: "2017-08-01", content: "But it's free"}
-    }
+    [
+    {title: "How about a hike?", date: "2017-08-03", content: "Cool weather, cloudy skies, beautiful views at Yosemite"},
+    {title: "Visit the museum?", date: "2017-08-05", content: "Rainy days don't have to ruin your plans!"},
+    {title: "Free concert!", date: "2017-08-01", content: "But it's free"}
+    ]
 
 
-
-  titles=[];
-  dates=[];
-  contents=[];
-
-//this breaks apart the JSON into arrays that angular can use to make the cards
   ngOnInit() {
-    for (let i=0; i<Object.keys(this.testcards).length; i++) {
-      this.titles[i]=this.testcards[i].title;
-      this.dates[i]=this.testcards[i].date;
-      this.contents[i]=this.testcards[i].content;
-    }
+    //sort that JSON list!
+    this.testcards.sort((a, b) => {
+    return new Date(a.date).getTime() - new Date(b.date).getTime()
+    });
   }
-
 
 }
