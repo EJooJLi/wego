61df859d60c5693c0ecf810543d88851d638c8c8
James Li <james.li@opower.com>
2017-08-28 23:41:56 -0700

    updating apiController

--- app/Controller/externalAPI/apiController.js
+++ app/Controller/externalAPI/apiController.js
@@ -4,13 +4,36 @@
 * Getting coordinates
 */
 
-// If user has a default address saved on profile & no perm coordinates
-// Call the Google Geocoder API and save the coordinates to perm coordinates
+var geocoder = NodeGeocoder({
+  provider: "google",
+  apiKey: "AIzaSyByfXpktliHp3ihiDCBRcDy8JU800DwFZ0",
+  httpAdapter: httpAdapter,
+  formatter: null
+});
 
-// If user has a default address saved on profile & has perm coordinates
-// Use the perm coordinates
 
 // If there are any overrides in address, use the cached override
+if (hasOverride) {
+  var coord = cachedCoord;
+}
+
+// If user has a default address saved on profile & no perm coordinates
+// Call the Google Geocoder API and save the coordinates to perm coordinates
+else if (defaultAddress && !permCoord) {
+  geocoder.geocode(req.address, function(err, res){
+  // Assigning latitude and longitude
+    var permCoord = {
+      lat: res[0].latitude,
+      lng: res[0].longitude
+    }
+    var coord = permCoord;
+  }
+}
+// If user has a default address saved on profile & has perm coordinates
+// Use the perm coordinates
+else if (defaultAddress && permCoord) {
+  var coord = permCoord;
+}
 
 /**
 * Getting weather data
