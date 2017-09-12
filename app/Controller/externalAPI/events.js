027599b310265f2279501143a7a8ed3e3cf4b2a3
James Li <james.li@opower.com>
2017-09-11 22:51:21 -0700

    update events api js

--- app/Controller/externalAPI/events.js
+++ app/Controller/externalAPI/events.js
@@ -1,36 +1,50 @@
 let express = require('express'); // For built-in middleware
 let request = require('request');
 
-// Testing by assiging coordinates - remove to do E2E Testing
-var coord = {
-  lat: 30,
-  lng: 55
-}
-
 // Declare eventapi information, consider moving this to .env
 let eventapi = {
   key: "&app_key=Msm3HTBsBfRVV6Ps",
-  root: "http://api.eventful.com/json/events/search?"
+  root: "http://api.eventful.com/json/events/search?",
+  radius: "&within=10&units=miles"
 }
 
 // Function to get Events information
-var getEvents = function(err, callback) {
-    if (err) {
-      console.log("Error!", err);
-      return;
-    } else {
-      // Create a coordinate string for events API
+// var getEvents = function(err, callback) {
+//     if (err) {
+//       console.log("Error!", err);
+//       return;
+//     } else {
+//       // Create a coordinate string for events API
+//       var coordString = "&location="+coord.lat+","+coord.lng;
+//       var eventsURL = `${eventapi.root}${coordString}${eventapi.radius}${eventapi.key}`
+//       request(eventsURL, function(error, response, body){
+//         console.log(JSON.parse(body));
+//         return body;
+//       });
+//     }
+// }
+
+module.exports = {
+  getEvents: function(coord) {
+    if (coord) {
       var coordString = "&location="+coord.lat+","+coord.lng;
       var eventsURL = `${eventapi.root}${coordString}${eventapi.radius}${eventapi.key}`
-      request(eventsURL, function(error, response, body){
-        console.log(JSON.parse(body));
-        return body;
-      });
+      request(eventsURL, function(err, response, body){
+        callback(null, body);
+      })
+    } else {
+        callback(new Error("No coords"));
     }
+  }
 }
 
-// Testing
-// getEvents(coord);
-
-// Export this out to use on apiController.js
-// module.exports = getEvents;
+// Defining a callback function to handle errors & responses
+var callback = function(err, data) {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log(JSON.parse(data[0]));
+    return;
+  }
+}
